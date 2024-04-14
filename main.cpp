66ca855daccaaea450abbb7e62a28d289972a8e4
Esteban <e.lopez7@hotmail.com>
2024-04-14 14:15:31 -0400

    Remove Build folder from repo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    QQuickStyle::setStyle("Material");
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
